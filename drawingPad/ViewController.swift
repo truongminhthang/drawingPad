ad9201be29caa0925652184b0f7462555e15a97c
Truong Thang <thangtruong.dev@gmail.com>
2018-02-22 15:40:17 +0700

    Initial Commit

---
+++ drawingPad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  drawingPad
+//
+//  Created by Thang Hoa on 2/22/18.
+//  Copyright © 2018 Thang Hoa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
